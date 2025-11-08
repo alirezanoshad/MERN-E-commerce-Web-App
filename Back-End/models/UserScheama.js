6a48729765cdd1b3f26ccab6b418caec1a3467a7
Hadixp <hadi.b.2002@gmail.com>
2025-11-09 00:52:30 +0330

    Create: user schema
    user schema created + hashing and compare middlewares

---
+++ Back-End/models/UserScheama.js
@@ -0,0 +1,26 @@
+// creating UserSchema
+const mongoose = require("mongoose");
+// importing bcrypt
+const bcrypt = require("bcrypt");
+const { trim, uniq } = require("lodash");
+// importing timestamp
+const timestamp = require('mongoose-timestamp');
+// defining UserScheama
+const userSchema = new mongoose.Schema({
+    name:{type:String,required:true,trim:true},
+    email:{type:String,required:true,unique:true,trim:true},
+    password:{type:String,required:true},
+    role:{type:String,enum:["customer","admin"],default:"customer"},
+})
+userSchema.plugin(timestamp);
+// bcrypt password
+userSchema.pre("save",async function(next){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+    next();
+})
+// match password
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
+module.exports = mongoose.model("User",userSchema);
