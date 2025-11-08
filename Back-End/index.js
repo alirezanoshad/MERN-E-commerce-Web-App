15577e219ec6e06012efaf84efa94211aeded2fe
Hadixp <hadi.b.2002@gmail.com>
2025-11-08 23:49:37 +0330

    added: imported express, cors, config
    imported some packages and running web server

---
+++ Back-End/index.js
@@ -0,0 +1,17 @@
+// importing packages:
+// express
+const express = require("express");
+// cors
+const cors = require("cors");
+// config
+const config = require("config");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+// creating web server
+app.listen(config.get("server.port"), () => {
+    console.log(`Server is running on port ${config.get("server.port")}`);
+});
