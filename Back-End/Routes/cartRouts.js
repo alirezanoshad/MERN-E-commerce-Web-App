5d97538c88511f5705d14c4f7990e712bfe140e6
Hadixp <hadi.b.2002@gmail.com>
2026-02-21 02:27:03 +0330

    Add: merge api
    this api merge guest and user cart

--- Back-End/Routes/cartRouts.js
+++ Back-End/Routes/cartRouts.js
@@ -185,6 +185,71 @@ cartRouter.get('/',async(req,res)=>{
     console.log(error);
     res.status(500).send('Server Error');
 }
+});
+
+// @route POST /api/cart/merge
+// @desc merge guest cart into user cart on login
+// @access private
+cartRouter.post('/merge',protect,async(req,res)=>{
+    // get guest user id
+    const guestUser = _.pick(req.body,['guestID']);
+    try {
+        // find the guest cart and user cart
+        const guestCart = await Cart.findOne({guestID:guestUser.guestID});
+        // now user cart
+        const userCart = await Cart.findOne({user:req.user._id});
+        // if guestCart is present so we check if the products are thre
+        if(guestCart){
+            if(guestCart.products.length === 0){
+                return res.status(400).json({msg:'guest cart is empty'});
+            }
+            if(userCart){
+                // merge guest cart into user cart
+                guestCart.products.forEach((guestItem)=>{
+                    const productIndex = userCart.products.findIndex((item)=> 
+                        item.productID.toString() === guestItem.productID.toString() &&
+                        item.size === guestItem.size &&
+                        item.color === guestItem.color
+            );
+
+            if(productIndex > -1){
+                // if the item is exist in the userCart update the quantity
+                userCart.products[productIndex].quantity += guestItem.quantity;
+            }else{
+                // otherwise add the guest item to the cart 
+                userCart.products.push(guestItem);
+            }
+
+                });
+
+                userCart.totalPrice = userCart.products.reduce((acc,item)=> acc + item.price * item.quantity,0);
+                await userCart.save();
+                // remove the guest cart after merging
+                try {
+                    await Cart.findOneAndDelete({guestID:guestUser.guestID});
+                } catch (error) {
+                    console.log(error);
+
+                }
+                res.status(200).json(userCart);
+            }else{
+                // if the user has no existing cart assign the guest cart to  the user
+                guestCart.user = req.user._id;
+                guestCart.guestID = undefined;
+                await guestCart.save();
+                res.status(200).json(guestCart);
+            }
+        }else{
+            if(userCart){
+                // guest cart has already been merged so return the user cart
+                return res.status(200).json(userCart)
+            }
+            res.status(404).json({msg:'guest cart not found'})
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Server Error")
+    }
 })
 
 
