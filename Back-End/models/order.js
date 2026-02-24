a8da3b1410c1fc62d1487880491387271eca0234
Hadixp <hadi.b.2002@gmail.com>
2026-02-25 00:50:16 +0330

    Add: order schema added
    this schema is for storing order data by users and it contains the user informations and product...

--- Back-End/models/order.js
+++ Back-End/models/order.js
@@ -1,4 +1,33 @@
 const mongoose = require('mongoose');
+const orderItemSchema = new mongoose.Schema({
+    productID:{type:mongoose.Schema.ObjectId,ref:'Product',required:true},
+    name:{type:String,required:true},
+    image:{type:String,required:true},
+    price:{type:Number,required:true},
+    size:String,
+    color:String,
+    quantity:{type:Number,required:true}
+
+},{_id:false});
+
 const orderSchema = new mongoose.Schema({
-    
-})
+    user:{type:mongoose.Schema.ObjectId,ref:'User',required:true},
+    orderItems:[orderItemSchema],
+    shippingAddress:{
+        address:{type:string,required:true},
+        city:{type:string,required:true},
+        postalCode:{type:string,required:true},
+        country:{type:string,required:true}
+    },
+    paymentMethod:{type:String,required:true},
+    totalPrice:{type:Number,required:true},
+    isPaid:{type:Boolean,default:false},
+    paidAt:{type:Date},
+    isDelivered:{type:Boolean,default:false},
+    deliveredAt:{type:Date},
+    paymentStatus:{type:string,default:'pending'},
+    status:{tyoe:String,enum:['Processing','Shipped','Delivered','Cancelled'],default:'Processing'},
+
+},{timestamps:true});
+
+module.exports = mongoose.model('Order',orderSchema);
