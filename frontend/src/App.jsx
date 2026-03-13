573aec1a926931a81b9d394f53c498b0a046e273
Alireza <iranexplosm@gmail.com>
2026-03-13 17:26:34 +0330

    Add sonner library to show toast notifications
    Install the 'sonner' toast library and enable it in the app: add sonner to frontend/package.json and mount a <Toaster position="top-right" /> in App.jsx (imported from sonner) to provide add-to-cart / notification UI. package-lock.json was updated to reflect dependency changes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserLayout } from "./components/Layout/UserLayout";
 import { Home } from "./pages/Home.jsx";
+// Sonner library - for add to cart notifications
+import { Toaster } from "sonner";
 
 // App component - 2 layouts considered: UserLayout & AdminLayout.
 // react-router-dom will be used to handle the routing between layouts.
@@ -8,6 +10,7 @@ export const App = () => {
   return (
     // URL Routing(react-router-dom)
     <BrowserRouter>
+      <Toaster position="top-right" />
       <Routes>
         {/* User Layout */}
         <Route path="/" element={<UserLayout />}>
