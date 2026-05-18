b5e5f79e9348b29a39ec0f79ef37466be7dcd28b
Hadixp <hadi.b.2002@gmail.com>
2026-05-19 02:01:33 +0430

    Add Subscriber Mongoose model
    Create Back-End/models/subscriber.js introducing a Subscriber Mongoose model. Defines email (String, required, unique, trimmed, lowercase) and a timestamp field sebscribedAt (Date, default Date.now()). Exports the model as 'Subscriber'. Note: the timestamp field name appears to be misspelled as 'sebscribedAt'.

---
+++ Back-End/models/subscriber.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+// creating schema
+const subscriberSchema = new mongoose.Schema({
+    email:{type:String,required:true,unique:true,trim:true,lowercase:true},
+    sebscribedAt:{type:Date,default:Date.now()},
+});
+
+module.exports = mongoose.model('Subscriber',subscriberSchema);
