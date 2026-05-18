d677d12089998a293f54562d6bc89469a9024343
Alireza <iranexplosm@gmail.com>
2026-05-18 17:42:33 +0330

    Add Admin layout, routes and imports
    Introduce AdminLayout and related admin page components to App.jsx and wire up nested /admin routes (AdminHomePage, UserManagement, ProductManagement, EditProductPage, OrderManagement). Also reorganize imports and comments, move the Toaster import, and clarify the UserLayout index route so the app supports a separate admin area with its own routing and pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
+// package Imports - react-router-dom(Dynamic Routing)
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// User Layout Imports
 import { UserLayout } from "./components/Layout/UserLayout";
 import { Home } from "./pages/Home.jsx";
 import { Login } from "./pages/Login.jsx";
 import { Register } from "./pages/Register.jsx";
 import { Profile } from "./pages/Profile.jsx";
 import { CollectionsPage } from "./pages/CollectionsPage.jsx";
-// Sonner library - for add to cart notifications
-import { Toaster } from "sonner";
 import { ProductDetails } from "./components/Products/ProductDetails.jsx";
 import { Checkout } from "./components/Cart/Checkout.jsx";
 import { OrderConfirmationPage } from "./pages/OrderConfirmationPage.jsx";
 import { OrderDetailsPage } from "./pages/OrderDetailsPage.jsx";
 import { MyOrders } from "./pages/MyOrders.jsx";
+
+// Sonner Library - for add to cart notifications
+import { Toaster } from "sonner";
+
+// Admin Layout Imports
+import { AdminLayout } from "./components/Admin/AdminLayout.jsx";
+import { AdminHomePage } from "./pages/AdminHomePage.jsx";
+import { UserManagement } from "./components/Admin/UserManagement.jsx";
+import { ProductManagement } from "./components/Admin/ProductManagement.jsx";
+import { EditProductPage } from "./components/Admin/EditProductPage.jsx";
+import { OrderManagement } from "./components/Admin/OrderManagement.jsx";
 
 // App component - 2 layouts considered: UserLayout & AdminLayout.
 // react-router-dom will be used to handle the routing between layouts.
@@ -21,7 +33,7 @@ export const App = () => {
     <BrowserRouter>
       <Toaster position="top-right" />
       <Routes>
-        {/* User Layout */}
+        {/* User Layout - with home page on index mode */}
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />}></Route>
           <Route path="login" element={<Login />}></Route>
@@ -42,7 +54,13 @@ export const App = () => {
         </Route>
 
         {/* Admin Layout */}
-        <Route></Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />}></Route>
+          <Route path="users" element={<UserManagement />}></Route>
+          <Route path="products" element={<ProductManagement />}></Route>
+          <Route path="products/:id/edit" element={<EditProductPage />}></Route>
+          <Route path="orders" element={<OrderManagement />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
