adb48b196cc73fdcbe0de3678acb317db7563526
Hadixp <hadi.b.2002@gmail.com>
2026-05-24 17:41:25 +0430

    Register admin router in backend
    Import the adminRoutes module and mount it at /api/admin in Back-End/index.js (adds: const adminRouter = require("./Routes/adminRoutes"); and app.use('/api/admin', adminRouter);). This enables admin-related endpoints to be served by the existing Express app.

--- Back-End/index.js
+++ Back-End/index.js
@@ -12,6 +12,7 @@ const cartRouter = require("./Routes/cartRouts");
 const checkOutRouter = require("./Routes/checkOutRouts");
 const orderRouter = require("./Routes/orderRoutes");
 const uploadRouter = require("./Routes/uploadRoute");
+const adminRouter = require("./Routes/adminRoutes");
 // importing database connection
 const connectDB = require("./config/DB");
 const app = express();
@@ -29,6 +30,7 @@ app.use("/api/cart",cartRouter);
 app.use('/api/checkOut',checkOutRouter)
 app.use('/api/order',orderRouter);
 app.use('/api/upload',uploadRouter);
+app.use('/api/admin',adminRouter);
 
 // creating web server
 app.listen(config.get("server.port"), () => {
