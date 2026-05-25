af1b9195828032614696c07f7f9a8090d95c3e40
Alireza <iranexplosm@gmail.com>
2026-05-25 20:45:46 +0330

    Wrap App with Redux Provider
    Expose the Redux store to the entire React app by importing Provider from react-redux and the store from ./redux/store.js, then wrapping the BrowserRouter/Routes tree with <Provider store={store}>. Also includes minor comment updates and formatting tweaks around routing and layout sections.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
-// package Imports - react-router-dom(Dynamic Routing)
+// URL Routing - react-router-dom(Dynamic Routing)
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Provider - Redux component
+// => Makes "store" Accessable by every Component(by wrapping the "App" component)
+import { Provider } from "react-redux";
+// Redux store refrence
+import store from "./redux/store.js";
 
 // User Layout Imports
 import { UserLayout } from "./components/Layout/UserLayout";
@@ -25,43 +31,49 @@ import { ProductManagement } from "./components/Admin/ProductManagement.jsx";
 import { EditProductPage } from "./components/Admin/EditProductPage.jsx";
 import { OrderManagement } from "./components/Admin/OrderManagement.jsx";
 
-// App component - 2 layouts considered: UserLayout & AdminLayout.
-// react-router-dom will be used to handle the routing between layouts.
+// App component - Contains 2 Layouts: UserLayout & AdminLayout.
 export const App = () => {
+  // note: Provider - Passes "store" prop, Enabling React Components to access Global States.
+  // note: react-router-dom - will be used to handle the routing between layouts.
+  // JSX
   return (
-    // URL Routing(react-router-dom)
-    <BrowserRouter>
-      <Toaster position="top-right" />
-      <Routes>
-        {/* User Layout - with home page on index mode */}
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="register" element={<Register />}></Route>
-          <Route path="profile" element={<Profile />}></Route>
-          <Route
-            path="collections/:collection"
-            element={<CollectionsPage />}
-          ></Route>
-          <Route path="product/:id" element={<ProductDetails />}></Route>
-          <Route path="checkout" element={<Checkout />}></Route>
-          <Route
-            path="order-confirmation"
-            element={<OrderConfirmationPage />}
-          ></Route>
-          <Route path="order/:id" element={<OrderDetailsPage />}></Route>
-          <Route path="my-orders" element={<MyOrders />}></Route>
-        </Route>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Toaster position="top-right" />
+        <Routes>
+          {/* User Layout - with homePage on index mode */}
+          <Route path="/" element={<UserLayout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="profile" element={<Profile />}></Route>
+            <Route
+              path="collections/:collection"
+              element={<CollectionsPage />}
+            ></Route>
+            <Route path="product/:id" element={<ProductDetails />}></Route>
+            <Route path="checkout" element={<Checkout />}></Route>
+            <Route
+              path="order-confirmation"
+              element={<OrderConfirmationPage />}
+            ></Route>
+            <Route path="order/:id" element={<OrderDetailsPage />}></Route>
+            <Route path="my-orders" element={<MyOrders />}></Route>
+          </Route>
 
-        {/* Admin Layout */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminHomePage />}></Route>
-          <Route path="users" element={<UserManagement />}></Route>
-          <Route path="products" element={<ProductManagement />}></Route>
-          <Route path="products/:id/edit" element={<EditProductPage />}></Route>
-          <Route path="orders" element={<OrderManagement />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          {/* Admin Layout */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminHomePage />}></Route>
+            <Route path="users" element={<UserManagement />}></Route>
+            <Route path="products" element={<ProductManagement />}></Route>
+            <Route
+              path="products/:id/edit"
+              element={<EditProductPage />}
+            ></Route>
+            <Route path="orders" element={<OrderManagement />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
