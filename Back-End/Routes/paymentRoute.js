23ae53ff093127373db161527185509057de15fe
Hadixp <hadi.b.2002@gmail.com>
2026-05-28 12:30:57 +0430

    Fix payment gateway request and config keys
    Update payment flow and gateway configuration: fix config key name (merchentId -> merchant_id) and set a new redirectUrl in default.json; update paymentRoute to require the Payment model, use config.get('gateway.merchant_id'), send the request with axios.post and return the gateway response. Added commented scaffolding for creating/saving a Payment record and redirecting the user after a successful authority, and cleaned up minor typos.

--- Back-End/Routes/paymentRoute.js
+++ Back-End/Routes/paymentRoute.js
@@ -3,6 +3,7 @@ const paymentRouter = express.Router();
 const _ = require('lodash');
 const axios = require('axios');
 const config = require("config");
+const Payment = require('../models/payment');
 
 
 // callback api
@@ -18,13 +19,25 @@ paymentRouter.post('/pay',async(req,res)=>{
         // distracting amount from body
     const amount = _.pick(req.body,["amount"]);
     const paymentData = {
-        merchant_id:config.get('gateway.merchentId'),
+        merchant_id:config.get('gateway.merchant_id'),
         amount:amount.amount,
         description:'buy product',
         callback_url:config.get('gateway.callBackUrl')
     };
-    const response = await axios(config.get('gateway.sandBox'),paymentData);
-    console.log(response.data);
+    const response = await axios.post(config.get('gateway.sandBox'),paymentData);
+    res.status(200).json(response.data);
+    // if(response.data.code == '100'){
+        // creating new payment
+        // const newPayment = new Payment({
+        //     user:req.user.id,
+        //     amount:amount.amount,
+        //     resNumber:response.data.authority
+        // })
+        // await newPayment.save();
+        // redirecting user
+        // res.redirect(config.get(`gateway.redirectUrl ${response.data.authority}`));
+        // console.log(config.get(`gateway.redirectUrl ${response.data.authority}`));
+    // }
 
     } catch (error) {
         console.log(error);
