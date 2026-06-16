0b5419d9481291c31c50ad12488f34fdad13af26
Hadixp <hadi.b.2002@gmail.com>
2026-06-17 01:28:57 +0430

    limiterUpdate

--- Back-End/middleware/rateLimit.js
+++ Back-End/middleware/rateLimit.js
@@ -3,9 +3,5 @@ const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, //every 15 min
 	limit: 10, // 10 try
 	message:'too many attemps', // the message to show
-    skip:(req,res)=>{
-        res.status(400).json({msg:'too many attemps'});
-        return false;
-    }
 });
 module.exports = limiter
